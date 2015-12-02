e34407e4c6b76f5ae3194252747aeabbff26aea4
Brooks J Rady <b.j.rady@gmail.com>
2015-12-02 15:09:58 -0700

    Add 'Node' class for storing building Traffic Graphs

---
+++ src/com/miekka/helper/Node.java
@@ -0,0 +1,59 @@
+package com.miekka.helper;
+
+public class Node {
+    private String ID;
+    private Pair<Double,Double> Position;
+    private long LastUpdate;
+    private double[] Velocity;
+    private double[] Heading;
+    //Add approx GPS position here.
+
+    public Node(String id, Pair<Double,Double> pos, double[] v, double[] h) {
+        this.ID = id;
+        this.Position = pos;
+        this.Velocity = v;
+        this.Heading = h;
+        this.LastUpdate = System.currentTimeMillis();
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public Pair<Double,Double> getPosition() {
+        return Position;
+    }
+
+    public long getLastUpdate() {
+        return LastUpdate;
+    }
+
+    public double[] getVelocity() {
+        return Velocity;
+    }
+
+    public double[] getHeading() {
+        return Heading;
+    }
+
+    public void updateNode(Pair<Double,Double> newPosition, double[] newVelocity, double[] newHeading) {
+        this.Position = newPosition;
+        this.Velocity = newVelocity;
+        this.Heading = newHeading;
+        this.LastUpdate = System.currentTimeMillis();
+    }
+
+    public String showNode() {
+        return (String.join("\n",
+                            "Information for node " + this.ID + ":",
+                            "\tPosition: " + this.Position.show(),
+                            "\tTime of last update (millis): " + this.LastUpdate,
+                            "\tCurrent Velocity: " + this.Velocity[0],
+                            "\tTarget Velocity: " + this.Velocity[1],
+                            "\tDelta Velocity (Acceleration): " + this.Velocity[2],
+                            "\tCurrent Heading: " + this.Heading[0],
+                            "\tTarget Heading: " + this.Heading[1],
+                            "\tDelta Heading (Turning Speed): " + this.Heading[2]));
+    }
+
+}
